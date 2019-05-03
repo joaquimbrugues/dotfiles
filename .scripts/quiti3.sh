c5c54b082de904e9ce8911f706cd957c8f9af22b
Joaquim <devjoaquim@gmail.com>
2019-05-03 13:49:33 +0200

    Add reboot option to quiti3 script, and add a prompt

--- .scripts/quiti3.sh
+++ .scripts/quiti3.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-MYVAR=`echo "Whoopsie\\ni3-quit\\nlinux-quit" | dmenu -l 3 | cut -d- -f1`
+MYVAR=`echo "Whoopsie\\ni3-quit\\nlinux-quit\\nreboot" | dmenu -p "Do you want to quit?" -l 4 | cut -d- -f1`
 if [ $MYVAR = "i3" ]; then
 	i3-msg exit
 fi
 if [ $MYVAR = "linux" ]; then
 	poweroff
+fi
+if [ $MYVAR = "reboot" ]; then
+	reboot
 fi
