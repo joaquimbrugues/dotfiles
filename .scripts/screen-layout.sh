17f4befcc39510fce0ecfcaab7c5d1d5b89f8a08
Joaquim <devjoaquim@gmail.com>
2019-05-06 16:29:40 +0200

    First version of the dmenu script to switch monitor configuration

---
+++ .scripts/screen-layout.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Use a dmenu to switch between diferent screen layouts with xrandr
+CHOICES="single\\ndual-right\\ndual-left"
+
+case $(dmenu -p "Monitor configuration:" -l 10 $CHOICES):
+	single) xrandr --output LVDS-1 --mode 1366x768 --output HDMI-1 --off && $HOME/.scripts/wallpaper.sh;;
+	dual-right) xrandr --output LVDS-1 --mode 1366x768 --pos 0x312 --primary --output HDMI-1 --mode 1920x1080 --pos 1366x0 && $HOME/.scripts/wallpaper.sh;;
+	dual-left) xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --primary --output HDMI-1 --mode 1920x1080 --pos 0x0 && $HOME/.scripts/wallpaper.sh
+	*) arandr && $HOME/.scripts/wallpaper.sh
+esac
