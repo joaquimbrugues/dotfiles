f31f8105f4c8e5003f30cb17e25e9299b1f1b6c1
Joaquim <devjoaquim@gmail.com>
2019-05-06 16:42:04 +0200

    Correct some scripts

--- .scripts/quiti3.sh
+++ .scripts/quiti3.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-MYVAR=`echo "Whoopsie\\ni3-quit\\nlinux-quit\\nreboot" | dmenu -p "Do you want to quit?" -l 4 | cut -d- -f1`
-if [ $MYVAR = "i3" ]; then
-	i3-msg exit
-fi
-if [ $MYVAR = "linux" ]; then
-	poweroff
-fi
-if [ $MYVAR = "reboot" ]; then
-	reboot
-fi
+# Quit from i3/power off the machine/reboot selecting the option from a dmenu
+OPTIONS="Whoopsie\\ni3-quit\\nlinux-quit\\nreboot"
+case $(echo $OPTIONS | dmenu -p "Do you want to quit?" -l 20 | cut -d- -f1) in
+	i3) i3-msg exit;;
+	linux) poweroff;;
+	reboot) reboot;;
+	*) ;;
+esac
--- .scripts/screen-layout.sh
+++ .scripts/screen-layout.sh
@@ -2,7 +2,7 @@
 # Use a dmenu to switch between diferent screen layouts with xrandr
 CHOICES="single\\ndual-right\\ndual-left"
 
-case $(dmenu -p "Monitor configuration:" -l 10 $CHOICES):
+case $(echo $CHOICES | dmenu -p "Monitor configuration:" -l 10) in
 	single) xrandr --output LVDS-1 --mode 1366x768 --output HDMI-1 --off && $HOME/.scripts/wallpaper.sh;;
 	dual-right) xrandr --output LVDS-1 --mode 1366x768 --pos 0x312 --primary --output HDMI-1 --mode 1920x1080 --pos 1366x0 && $HOME/.scripts/wallpaper.sh;;
 	dual-left) xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --primary --output HDMI-1 --mode 1920x1080 --pos 0x0 && $HOME/.scripts/wallpaper.sh
