8046865410c80f722358de3fe57571f5d39e25ff
Joaquim <devjoaquim@gmail.com>
2019-05-08 00:23:25 +0200

    Improve the man-this.sh script
    Whenever the user exited man-this without picking an option, an instance of Zathura launched with an empty document. An if has been added to prevent this.

--- .scripts/man-this.sh
+++ .scripts/man-this.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-# This script was shamelessly copied from Luke Smith's dotfiles
-man -k . | dmenu -p "Show man" -l 10 | awk '{print $1}' | xargs -r man -Tpdf | zathura -
+# This script is based in Luke Smith's ideas
+# Print the list of all available manuals to dmenu, and then print the picked manual to pdf and show it in Zathura
+MANUAL=$(man -k . | dmenu -p "Show man" -l 10 | awk '{print $1}')
+
+if [ -n "$MANUAL" ]
+then
+	man -Tpdf $MANUAL | zathura -
+fi
