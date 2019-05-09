05664d5623fd81435b06ef88b87fe05df78acd91
Joaquim <devjoaquim@gmail.com>
2019-05-10 01:53:56 +0200

    Adapt shutdown and reboot commands to Debian poweroff

--- .scripts/quiti3.sh
+++ .scripts/quiti3.sh
@@ -3,7 +3,9 @@
 OPTIONS="Whoopsie\\ni3-quit\\nlinux-quit\\nreboot"
 case $(echo $OPTIONS | dmenu -p "Do you want to quit?" -l 20 | cut -d- -f1) in
 	i3) i3-msg exit;;
-	linux) poweroff;;
-	reboot) reboot;;
+	#linux) poweroff;;
+	linux) systemctl poweroff;;
+	#reboot) reboot;;
+	reboot) systemctl reboot;;
 	*) ;;
 esac
