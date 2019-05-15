1f52911562371c5f24b7f3a3ac144e40c2763774
Joaquim <devjoaquim@gmail.com>
2019-05-16 00:18:24 +0200

    Improve presentation of a script

--- .scripts/man-this.sh
+++ .scripts/man-this.sh
@@ -2,8 +2,4 @@
 # This script is based in Luke Smith's ideas
 # Print the list of all available manuals to dmenu, and then print the picked manual to pdf and show it in Zathura
 MANUAL=$(man -k . | dmenu -p "Show man" -l 10 | awk '{print $1}')
-
-if [ -n "$MANUAL" ]
-then
-	man -Tpdf $MANUAL | zathura -
-fi
+[ -n "$MANUAL" ] && man -Tpdf $MANUAL | zathura -
