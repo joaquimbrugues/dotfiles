c5f37627762f11e9f37e4af0bf22d4cc0a0e5233
Joaquim <devjoaquim@gmail.com>
2019-05-22 00:24:51 +0200

    Edit the script to add bibliography items to create the file if needed

--- .scripts/latex/addtobib.sh
+++ .scripts/latex/addtobib.sh
@@ -7,7 +7,11 @@ if [ -z "$ID" ]
 then
 	notify-send "Malformed input"
 else
-	printf "\n" >> $BIB
+	# If the bibliography exists, add a newline
+	[ -w $BIB ] && printf "\n" >> $BIB
+	# If the bibliography file does not exist, create it
+	[ ! -w $BIB ] && touch $BIB
+	# Format the bibliography item and output it to the file
 	echo "$(echo $ITEM | sed -e 's/, /,\n\t/g' -e 's/} /}\n/')" >> $BIB
 	notify-send "$ID added to the bibliography"
 fi
